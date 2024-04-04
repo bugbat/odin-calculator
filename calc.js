9760d3b2ed51719c1738933e144ff159867e80c8
bugbat <85291787+bugbat@users.noreply.github.com>
2024-04-03 20:00:58 -0400

    Basic css + html and buttons are inputting to screen

--- calc.js
+++ calc.js
@@ -16,3 +16,41 @@ function multiply(a, b) {
   return a * b;
 }
 
+// display
+const display = document.querySelector("#calc-screen p")
+
+function updateDisplay(input) {
+  if (display.textContent === "0") {
+    display.textContent = input;
+  }
+  else {
+    display.textContent += input;
+  }
+}
+
+function clearDisplay() {
+  display.textContent = "0";
+}
+
+// buttons
+const buttons = document.querySelectorAll("button");
+
+function buttonSelect(button) {
+  const type = button.className;
+  const value = button.value;
+  console.log("Value " + button.value + ", Class " + button.className);
+
+  if (value === "clear") {
+    clearDisplay();
+  }
+
+  if (type === "operand") {
+    updateDisplay(button.value);
+  }
+}
+
+buttons.forEach (button => {
+  button.addEventListener("click", function() {
+    buttonSelect(button)
+  });
+});
